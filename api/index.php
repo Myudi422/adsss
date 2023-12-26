5176590a6923fbb29427528690851db5c673bfd5
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-12-26 08:25:01 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,52 +5,59 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Download Aplikasi Update</title>
-  <!-- Bootstrap CSS CDN -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    integrity="sha384-rbs5gXsOi5qzKKO5cWg5fA5YEtjMHP6eg5tHT4U/8F+pDDiQHHjNakc3JZExzN" crossorigin="anonymous">
+  <!-- Tailwind CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-gray-100">
 
-  <div class="container mt-5">
-    <h2 class="text-center mb-4">Pilih Tipe ARM</h2>
+  <div class="container mx-auto mt-8">
+    <h2 class="text-3xl font-semibold text-center mb-8">Pilih Tipe ARM</h2>
 
-    <div class="row justify-content-center">
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">ARMv7</h5>
-            <p class="card-text">Download Aplikasi untuk ARMv7</p>
-            <a href="link_update_armv7.apk" class="btn btn-primary">Download</a>
-          </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div class="max-w-sm mx-auto bg-white rounded overflow-hidden shadow-lg">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">ARMv7</div>
+          <p class="text-gray-700 text-base">Download Aplikasi untuk ARMv7</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <a href="link_update_armv7.apk"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Download
+          </a>
         </div>
       </div>
 
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">ARM64</h5>
-            <p class="card-text">Download Aplikasi untuk ARM64</p>
-            <a href="link_update_arm64.apk" class="btn btn-primary">Download</a>
-          </div>
+      <div class="max-w-sm mx-auto bg-white rounded overflow-hidden shadow-lg">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">ARM64</div>
+          <p class="text-gray-700 text-base">Download Aplikasi untuk ARM64</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <a href="link_update_arm64.apk"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Download
+          </a>
         </div>
       </div>
 
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">x86</h5>
-            <p class="card-text">Download Aplikasi untuk x86</p>
-            <a href="link_update_x86.apk" class="btn btn-primary">Download</a>
-          </div>
+      <div class="max-w-sm mx-auto bg-white rounded overflow-hidden shadow-lg">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">x86</div>
+          <p class="text-gray-700 text-base">Download Aplikasi untuk x86</p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <a href="link_update_x86.apk"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Download
+          </a>
         </div>
       </div>
     </div>
   </div>
 
-  <!-- Bootstrap JS and Popper.js CDN -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-bt6IsA7OI/pHJ93vPXvnnz4TEOysbJ7H9mVbSSXyU5qo7fpjcmFODPPw87jAu92L" crossorigin="anonymous"></script>
+  <!-- Optional: Add your own script or additional CDN here -->
+
 </body>
 
 </html>
