20cac1bf0a624d46dcf940dbb10134eee6715690
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-12-26 17:22:02 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -23,7 +23,7 @@
       <div class="max-w-md mx-auto bg-blue-200 rounded p-4">
         <h3 class="text-xl font-semibold mb-2 text-center">ARMv7</h3>
         <p class="text-gray-700 text-base mb-4 text-center">Pilihan terbaik untuk perangkat ARMv7.</p>
-        <a href="link_update_armv7.apk"
+        <a href="https://subjective-tobye-myudi422.koyeb.app/17978"
           class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center">
           Download
         </a>
@@ -33,7 +33,7 @@
       <div class="max-w-md mx-auto bg-green-200 rounded p-4">
         <h3 class="text-xl font-semibold mb-2 text-center">ARM64</h3>
         <p class="text-gray-700 text-base mb-4 text-center">Untuk perangkat dengan tipe ARM64.</p>
-        <a href="link_update_arm64.apk"
+        <a href="https://subjective-tobye-myudi422.koyeb.app/17977"
           class="block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full text-center">
           Download
         </a>
@@ -43,7 +43,7 @@
       <div class="max-w-md mx-auto bg-yellow-200 rounded p-4">
         <h3 class="text-xl font-semibold mb-2 text-center">x86</h3>
         <p class="text-gray-700 text-base mb-4 text-center">Kompatibel dengan perangkat x86.</p>
-        <a href="link_update_x86.apk"
+        <a href="https://subjective-tobye-myudi422.koyeb.app/17979"
           class="block bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full text-center">
           Download
         </a>
@@ -54,9 +54,7 @@
     <div class="mt-8">
       <h3 class="text-xl font-semibold mb-4">Catatan Pembaruan</h3>
       <ul class="list-disc pl-6">
-        <li>Fitur baru yang luar biasa</li>
-        <li>Peningkatan kinerja aplikasi</li>
-        <li>Perbaikan bug yang signifikan</li>
+        <li>Fix Bug Browse Page</li>
       </ul>
     </div>
   </div>
