28acf2ab4d7197450b92f5adb42ea7946588980e
akuiiki <43848105+Myudi422@users.noreply.github.com>
2023-12-26 08:23:02 +0700

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Download Aplikasi Update</title>
+  <!-- Bootstrap CSS CDN -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    integrity="sha384-rbs5gXsOi5qzKKO5cWg5fA5YEtjMHP6eg5tHT4U/8F+pDDiQHHjNakc3JZExzN" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container mt-5">
+    <h2 class="text-center mb-4">Pilih Tipe ARM</h2>
+
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">ARMv7</h5>
+            <p class="card-text">Download Aplikasi untuk ARMv7</p>
+            <a href="link_update_armv7.apk" class="btn btn-primary">Download</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">ARM64</h5>
+            <p class="card-text">Download Aplikasi untuk ARM64</p>
+            <a href="link_update_arm64.apk" class="btn btn-primary">Download</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">x86</h5>
+            <p class="card-text">Download Aplikasi untuk x86</p>
+            <a href="link_update_x86.apk" class="btn btn-primary">Download</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap JS and Popper.js CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-bt6IsA7OI/pHJ93vPXvnnz4TEOysbJ7H9mVbSSXyU5qo7fpjcmFODPPw87jAu92L" crossorigin="anonymous"></script>
+</body>
+
+</html>
