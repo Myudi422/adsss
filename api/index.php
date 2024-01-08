13ead5d5b1106ad27f21d88c109a745964c0b9af
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-01-08 11:02:33 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,3 +3,7 @@
 header("Location: https://raw.githubusercontent.com/Myudi422/apk-niflex/main/app-release.apk");
 exit(); // Ensure that no other content is sent
 ?>
+<script>
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+</script>
+<script defer src="/_vercel/insights/script.js"></script>
