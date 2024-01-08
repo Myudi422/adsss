038eac07cb7999dd5fe2f6381052ee74072c8b87
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-01-08 11:07:38 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,12 +1,4 @@
 <?php
-// Include your JavaScript code here
-?>
-<script>
-  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
-</script>
-<script defer src="/_vercel/insights/script.js"></script>
-
-<?php
 // Redirect to the specified URL
 header("Location: https://raw.githubusercontent.com/Myudi422/apk-niflex/main/app-release.apk");
 exit(); // Ensure that no other content is sent
