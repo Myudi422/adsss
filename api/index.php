5b9429d5f1494714d8117125a33ab272a4e6f899
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-08-02 11:14:08 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,15 @@
 <?php
-// Redirect to the specified URL
-header("Location: https://play.google.com/store/apps/details?id=com.nextccgmedia.flue");
+// List of URLs
+$urls = [
+    "https://www.highratecpm.com/j10z40hvf0?key=c9b7d12c462d2d6b4c6693a0de308ffa",
+    "https://www.highratecpm.com/wnsqkxka?key=1cd0a432fe5974344f2cff5179d9951b",
+    // Add more URLs as needed
+];
+
+// Select a random URL
+$randomUrl = $urls[array_rand($urls)];
+
+// Redirect to the selected URL
+header("Location: $randomUrl");
 exit(); // Ensure that no other content is sent
 ?>
